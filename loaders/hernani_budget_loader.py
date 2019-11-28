7be222a7e45dba85b73a12a2be2e80024ab45155
Eduardo Sebastian <esebastian@altiora.es>
2019-11-28 02:08:12 +0100

    Update budget loader
    Refactors the budget loader to extract all necessary codes from the
    always present budget line and ignore the bridged amounts coming from
    previous years.
    
    Also, removes the existing institutional hierarchy as it wasn't correct
    and assign all expenses and income to the root node.

--- loaders/hernani_budget_loader.py
+++ loaders/hernani_budget_loader.py
@@ -3,11 +3,11 @@ from budget_app.loaders import SimpleBudgetLoader
 
 
 expenses_mapping = {
-    'default': {'ic_code': 17, 'fc_code': 13, 'full_ec_code': 0, 'description': 2, 'forecast_amount': 4, 'actual_amount': 9},
+    'default': {'budget_line': 0, 'description': 2, 'budgeted_amount': 4, 'executed_amount': 9},
 }
 
 income_mapping = {
-    'default': {'ic_code': None, 'full_ec_code': 0, 'description': 3, 'forecast_amount': 4, 'actual_amount': 5},
+    'default': {'budget_line': 0, 'description': 3, 'budgeted_amount': 4, 'executed_amount': 5},
 }
 
 programme_mapping = {
@@ -27,12 +27,10 @@ class BudgetCsvMapper:
         if not mapping:
             mapping = column_mapping.get('default')
 
-        self.ic_code = mapping.get('ic_code')
-        self.fc_code = mapping.get('fc_code')
-        self.full_ec_code = mapping.get('full_ec_code')
+        self.budget_line = mapping.get('budget_line')
         self.description = mapping.get('description')
-        self.forecast_amount = mapping.get('forecast_amount')
-        self.actual_amount = mapping.get('actual_amount')
+        self.budgeted_amount = mapping.get('budgeted_amount')
+        self.executed_amount = mapping.get('executed_amount')
 
 
 class HernaniBudgetLoader(SimpleBudgetLoader):
@@ -50,40 +48,50 @@ class HernaniBudgetLoader(SimpleBudgetLoader):
         # Mapper
         mapper = BudgetCsvMapper(self.year, is_expense)
 
+        # Budget line
+        # We get the full budget line, so we have to split and amend
+        budget_line = line[mapper.budget_line].split(" ")
+        budget_line_codes = budget_line[1].split(".")
+        budget_line_year = budget_line[2]
+
+        # There are budget lines from past years, that require some special treatment
+        # as shouldn't be included in the budget or executed amounts
+        is_past_budget_line = int(budget_line_year) < int(self.year)
+
+        if is_past_budget_line:
+            return None
+
+        # Income budget lines codes doesn't include institutional ones, and that
+        # changes the shape of the codes string, so we need to amend it to allow
+        # uniform processing
+        if len(budget_line_codes) < 5:
+            budget_line_codes.insert(0, '00')
+
+        # Economic code
+        ec_code = budget_line_codes[1]
+
+        # Item numbers are the last two digits from the economic codes (fourth and fifth digits)
+        item_number = budget_line_codes[2]
+
+        # Institutional code
+        # We got 2- digit codes (budget_line_codes[0]), that would correspond to
+        # institutional codes, but there doesn't seem to be a clear correspondance with
+        # entity sections, so all expenses and income goes to the root node
+        ic_code = '000'
+
+        # Functional code
+        # We got 5- digit functional codes as input or nothing for some income data
+        fc_code = "".join(budget_line_codes[3:5])
+
         # Description
         description = line[mapper.description].strip()
 
         # Parse amount
-        amount = line[mapper.actual_amount if is_actual else mapper.forecast_amount]
+        amount = line[mapper.executed_amount if is_actual else mapper.budgeted_amount]
         amount = self._parse_amount(amount)
 
         # Expenses
         if is_expense:
-            # Institutional code
-            # We got 2- or 4- digit institutional codes as input, but we only
-            # need the first two, although we need to be able to represent the
-            # number in just one char and then get a three digit code
-            ic_code = line[mapper.ic_code].strip()
-            ic_code = hex(int(ic_code[:2]))[2:].upper()
-            ic_code = '0' + ic_code + '0'
-
-            # Economic code
-            # We get the full budget line, so we have to split and amend
-            full_ec_code = line[mapper.full_ec_code].strip()
-            full_ec_code = full_ec_code.split(" ")[1:-1][0]
-            full_ec_code = full_ec_code.split(".")[1:3]
-            full_ec_code = "".join(full_ec_code)
-
-            # Concepts are the first three digits from the economic codes
-            ec_code = full_ec_code[:3]
-
-            # Item numbers are the last two digits from the economic codes (fourth and fifth digits)
-            item_number = full_ec_code[-2:]
-
-            # Functional code
-            # We got 5- digit functional codes as input
-            fc_code = line[mapper.fc_code].strip()
-
             # Programme codes have changed in 2015, due to new laws. Since the application expects a code-programme
             # mapping to be constant over time, we are forced to amend budget data prior to 2015.
             # See https://github.com/dcabo/presupuestos-aragon/wiki/La-clasificaci%C3%B3n-funcional-en-las-Entidades-Locales
@@ -93,23 +101,6 @@ class HernaniBudgetLoader(SimpleBudgetLoader):
 
         # Income
         else:
-            # Institutional code
-            # All income goes to the root node
-            ic_code = '000'
-
-            # Economic code
-            # We get the full budget line, so we have to split and amend
-            full_ec_code = line[mapper.full_ec_code].strip()
-            full_ec_code = full_ec_code.split(" ")[1:-1][0]
-            full_ec_code = full_ec_code.split(".")[:2]
-            full_ec_code = "".join(full_ec_code)
-
-            # Concepts are the first three digits from the economic codes
-            ec_code = full_ec_code[:3]
-
-            # Item numbers are the last two digits from the economic codes (fourth and fifth digits)
-            item_number = full_ec_code[-2:]
-
             # Functional code
             # We don't need functional codes in income
             fc_code = None
